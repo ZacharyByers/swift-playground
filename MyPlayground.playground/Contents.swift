a8acbe7620e97fe0472bd9dd69338c7a3d684fe6
Zach Byers <byerszach@gmail.com>
2018-01-30 20:15:58 -0700

    for loop enumerable

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,8 +2,10 @@
 
 import UIKit
 
-var array = ["Billy", "Bob", "Joe", "Jim"]
+var array = [8, 7.0, 19, 28]
 
-for name in array {
-  print("Hi there \(name)!")
+for (index, value) in array.enumerated() {
+  array[index] = value / 2
 }
+
+print(array)
