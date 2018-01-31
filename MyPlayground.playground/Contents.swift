19f5f8b3c55d3604c0ba5857846ceae41213632b
Zach Byers <byerszach@gmail.com>
2018-01-30 20:05:58 -0700

    for loops

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,10 +2,8 @@
 
 import UIKit
 
-var array = [3.87, 7.1, 8.9]
-array.remove(at: 1)
-array.append(array[0] * array[1])
-print(array)
+var array = ["Billy", "Bob", "Joe", "Jim"]
 
-let dictionary = ["pizza": 10.99, "ice cream": 4.99, "salad": 7.99]
-print("The total cost of my meal is \(dictionary["pizza"]! + dictionary["salad"]!)")
+for name in array {
+  print("Hi there \(name)!")
+}
