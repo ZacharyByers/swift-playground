4388885ff93e112f70117f414c1f1b4d0ec43149
Zach Byers <byerszach@gmail.com>
2018-01-30 19:26:08 -0700

    more

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -7,3 +7,11 @@ let name = "Zach"
 let age = "25"
 
 print("My name is \(name) and I am \(age) years old")
+
+let dub = 5.76
+
+let inty = 8
+
+let product = Double(inty) * dub
+
+print("The product of \(dub) and \(inty) is \(product)")
